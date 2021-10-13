1b292840adb0123d21c3c63ec55e43d9b43273cb
Holamundo2020 <ac0096032016@unab.edu.sv>
2021-10-13 16:03:38 -0600

    https://github.com/Holamundo2020/Proyecto4.git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { DummyComponent } from './dummy/dummy.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path: 'Home', component: DummyComponent},
+{path: 'Services', component: DummyComponent},
+{path: 'Proyects', component: DummyComponent},
+{path: 'Aboutus', component: DummyComponent},
+{path: 'signup', component: DummyComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
